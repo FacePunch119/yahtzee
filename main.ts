97305f23d7d9185c232c401f36195a1f6892d3f5
FacePunch119 <92817162+FacePunch119@users.noreply.github.com>
2021-11-08 14:19:14 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,32 +4,54 @@ let Stored_Dice_1 = 0
 let Stored_Dice_5 = 0
 let Stored_Dice_2 = 0
 let Stored_Dice_4 = 0
+let Shake_Count = 0
 let Dice_1 = 0
 let Dice_2 = 0
 let Dice_3 = 0
 let Dice_4 = 0
 let Dice_5 = 0
 input.onPinPressed(TouchPin.P0, function () {
-    basic.showIcon(IconNames.Yes)
-    Stored_Dice_3 = Stored
+    if (Stored_Dice_3 > 0) {
+        Stored_Dice_3 = 0
+    } else {
+        basic.showIcon(IconNames.Yes)
+        Stored_Dice_3 = Stored
+    }
 })
 input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Yes)
-    Stored_Dice_1 = Stored
+    if (Stored_Dice_1 > 0) {
+        Stored_Dice_1 = 0
+    } else {
+        basic.showIcon(IconNames.Yes)
+        Stored_Dice_1 = Stored
+    }
 })
 input.onPinPressed(TouchPin.P2, function () {
-    basic.showIcon(IconNames.Yes)
-    Stored_Dice_5 = Stored
+    if (Stored_Dice_5 > 0) {
+        Stored_Dice_5 = 0
+    } else {
+        basic.showIcon(IconNames.Yes)
+        Stored_Dice_5 = Stored
+    }
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.Yes)
-    Stored_Dice_2 = Stored
+    if (Stored_Dice_2 > 0) {
+        Stored_Dice_2 = 0
+    } else {
+        basic.showIcon(IconNames.Yes)
+        Stored_Dice_2 = Stored
+    }
 })
 input.onPinPressed(TouchPin.P1, function () {
-    basic.showIcon(IconNames.Yes)
-    Stored_Dice_4 = Stored
+    if (Stored_Dice_4 > 0) {
+        Stored_Dice_4 = 0
+    } else {
+        basic.showIcon(IconNames.Yes)
+        Stored_Dice_4 = Stored
+    }
 })
 input.onGesture(Gesture.Shake, function () {
+    Shake_Count = Shake_Count + 1
     Dice_1 = randint(1, 6)
     Dice_2 = randint(1, 6)
     Dice_3 = randint(1, 6)
@@ -37,14 +59,9 @@ input.onGesture(Gesture.Shake, function () {
     Dice_5 = randint(1, 6)
 })
 basic.forever(function () {
-    basic.showString("D1")
-    basic.showNumber(Dice_1)
-    basic.showString("D2")
-    basic.showNumber(Dice_2)
-    basic.showString("D3")
-    basic.showNumber(Dice_3)
-    basic.showString("D4")
-    basic.showNumber(Dice_4)
-    basic.showString("D5")
-    basic.showNumber(Dice_5)
+    if (true) {
+    	
+    } else {
+    	
+    }
 })
